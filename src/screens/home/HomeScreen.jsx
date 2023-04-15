7c744045b8889faf5a6d22bdc4357784252f9d85
Barbara Verdur <barbiverdur@gmail.com>
2023-04-14 21:41:20 -0300

    home-image

--- src/screens/home/HomeScreen.jsx
+++ src/screens/home/HomeScreen.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
-import { SafeAreaView, Text } from 'react-native'
+import { ImageBackground, SafeAreaView, Text } from 'react-native'
 import { styles } from './HomeScreen.styles'
 
 export const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>
-        Consulta el Calendario de Jujuy y enterate de todos los eventos!
-      </Text>
+      <ImageBackground
+        style={styles.bgImage}
+        source={require("../../../assets/main.jpg")}
+      >
+        <Text style={styles.title}>
+          Consulta el Calendario de Jujuy y enterate de todos los eventos!
+        </Text>
+      </ImageBackground>
     </SafeAreaView>
-  )
+  );
 }
