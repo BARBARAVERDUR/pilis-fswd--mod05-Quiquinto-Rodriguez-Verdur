dc8de24772ed9243ef5afd239c6776f825ba8f34
Valeria Quiquinto <valeria.quiquinto@gmail.com>
2023-04-15 13:43:01 -0300

    feat(components): Fav
    se agrego las propiedades "isFav" (si es favorito o no) y el
    "hadlerPress" es una funcion que se ejecuta cada vez que presionamos
    el componente.

--- src/components/Fav/Fav.js
+++ src/components/Fav/Fav.js
@@ -3,19 +3,20 @@ import { Ionicons } from '@expo/vector-icons'
 import { COLORS } from '../../utils'
 import { Pressable } from 'react-native' 
 
-export function Fav () {
+export function Fav ({isFav, handlePress=()=>[]}) {
     const [color, setColor] = useState(COLORS.inactive)
 
-    const handleClick = () => {
-        if (color===COLORS.inactive) {
+    const onPress = () => {
+        if (isFav) {
             setColor(COLORS.primary)
-        }else{
+        }else{ 
             setColor(COLORS.inactive)
         }
+        handlePress()
     }
 
     return (
-        <Pressable onPress={handleClick}>
+        <Pressable onPress={onPress}>
             <Ionicons name='star' size={20} color={color}/>
         </Pressable>
     )
--- src/screens/event-detail/layouts/Detail.layouts.jsx
+++ src/screens/event-detail/layouts/Detail.layouts.jsx
@@ -2,18 +2,25 @@ import { Text, View } from 'react-native'
 import { styles } from '../LocationDetailScreen.styles'
 import { Fav } from '../../../components'
 import { useUser } from '../../../contexts/UserContext' 
+import { useState } from 'react'
 
 export const Detail = ({ event }) => {
   const { place } = event
   const { currentUser } = useUser()
+  const [isFav, setFav] = useState(false)
+
   return (
     <View style={styles.textContainer}>
       <Text style={styles.title}>{event.name}</Text>
       <Text style={styles.price}>{place.name}</Text>
       <Text style={styles.location}>{place.direction}</Text>
-      {currentUser!==null && (
+      {currentUser!==null ? (
         <View style={styles.ratingContainer}>
-          <Fav/>
+          <Fav isFav={isFav} handlePress={()=>{setFav(isFav=>!isFav)}}/>
+        </View>
+      ):(
+        <View style={styles.ratingContainer}>
+          <Fav isFav={false}/>
         </View>
       )}
       <Text style={styles.description}>{event.description}</Text>
