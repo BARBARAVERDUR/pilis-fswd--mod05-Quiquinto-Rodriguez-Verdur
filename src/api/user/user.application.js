8cfd863c0ef59677cb105502411d7d5eb26f8463
red-mat <jesus.ing.rodriguez@gmail.com>
2023-04-21 16:44:42 -0300

    feat(api): user
    se agrego localAuthService en la capa de aplicacion

--- src/api/user/user.application.js
+++ src/api/user/user.application.js
@@ -1,10 +1,15 @@
-import { DevRepository } from './user.infrastructure'
+import { DevRepository, StorageRepository } from './user.infrastructure'
 import { AuthService } from './user.model'
 
 const repository = new DevRepository()
 const authService = new AuthService(repository)
+
+const localRepository = new StorageRepository()
+const localAuthServices = new AuthService(localRepository)
 export class User {
   static async LogIn (username, password) {
+    const localUser = await localAuthServices.auth(username, password)
+    if (localUser !== null) return localUser
     return await authService.auth(username, password)
   }
 }
