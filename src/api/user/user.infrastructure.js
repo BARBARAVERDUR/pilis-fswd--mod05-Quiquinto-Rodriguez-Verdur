342525eb6e61bb732450a882f8efc3de24c2e4fb
red-mat <jesus.ing.rodriguez@gmail.com>
2023-04-21 16:39:45 -0300

    feat(api): users
    Se agrego la infraestructura StorageRepository

--- src/api/user/user.infrastructure.js
+++ src/api/user/user.infrastructure.js
@@ -1,3 +1,4 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import { api } from '../shared/api'
 import { UserModel, UserRepository } from './user.model'
 
@@ -14,6 +15,34 @@ export const getUsers = async () => {
 export class DevRepository extends UserRepository {
   async auth (username, password) {
     const users = await getUsers()
+    const user = users.filter(user => user.username === username)[0]
+
+    if (user === undefined) return null
+    if (password !== user.password) return null
+
+    return new UserModel(
+      user.id,
+      user.username,
+      user.avatar,
+      user.description,
+      user.favorites
+    )
+  }
+}
+
+const KEY_USERS = '@users'
+export const getUsersStorage = async () => {
+  const usersStorage = await AsyncStorage.getItem(KEY_USERS)
+  if (usersStorage == null) {
+    await AsyncStorage.setItem(KEY_USERS, '[]')
+    return []
+  }
+  return JSON.parse(usersStorage)
+}
+
+export class StorageRepository extends UserRepository {
+  async auth (username, password) {
+    const users = await getUsersStorage()
     const user = users.filter(user => user.username === username)[0]
 
     if (user === undefined) return null
